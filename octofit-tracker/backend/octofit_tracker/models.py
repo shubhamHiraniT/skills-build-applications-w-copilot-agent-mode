89b9f331e200aa53e46ea97ce2fc27d6f4378d41
shubhamHiraniT <shubham.hirani@talentica.com>
2026-03-10 05:59:25 +0000

    Add initial Django project structure with models, views, serializers, and migrations

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+
+    def __str__(self):
+        return self.name
+
+class Activity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
+    type = models.CharField(max_length=100)
+    duration = models.IntegerField()  # in minutes
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.type} - {self.user.name}"
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    suggested_for = models.ManyToManyField(User, related_name='workouts')
+
+    def __str__(self):
+        return self.name
+
+class Leaderboard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.name}: {self.score}"
